11f7dd2e53deca3d6c3137cf41d9a53816ed6b99
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-22 22:33:56 +0900

    supabaseアップロード機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { supabase } from "./utils/supabace";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleUpload = async () => {
+    if (!file) return;
+    const { data, error } = await supabase.storage
+      .from("pictures")
+      .upload(`Clothes/${file.name}`, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+    if (error) {
+      console.error("Error uploading file:", error);
+    } else {
+      console.log("File uploaded successfully:", data);
+    }
+  };
+
+  const onchangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files![0]);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="file" onChange={onchangeImage} />
+        <button onClick={handleUpload}>アップロードする</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
